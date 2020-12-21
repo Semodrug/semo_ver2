37e215ca3f3934e398974eba69ab7ae87352eff3
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-12-21 17:12:13 +0900

    Set settings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:semo_ver2/main.dart';
 
+import '../lib/home/home.dart';
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
