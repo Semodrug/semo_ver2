9e749fd35ddb6172c079ba5e32000e51e139818e
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-12-22 10:34:08 +0900

    Update login.dart

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-import '../home/home.dart';
-import 'register_1.dart';
+import 'package:semo_ver2/login/register_1.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 // final GoogleSignIn _googleSignIn = GoogleSignIn();
@@ -18,67 +14,12 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        /* leading ==> 로그인 페이지 뛰어 넘을 수 있게 만들어놓은 버튼 */
-        leading: IconButton(
-          icon: Icon(Icons.android),
-          color: Colors.white,
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage() //
-                    //CategoryMenu()
-                    ));
-
-            //Navigator.pushNamed(context, '/HomeDrug');
-          },
-        ),
-        centerTitle: true,
-        //title: Text(widget.title),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                Colors.teal[100],
-                //Colors.teal[50],
-                Colors.teal[200]
-              ])),
-        ), //gradation
-        actions: <Widget>[
-          Builder(builder: (BuildContext context) {
-            return FlatButton(
-              child: const Text('Log out'),
-              textColor: Theme.of(context).buttonColor,
-              onPressed: () async {
-                final User user = await _auth.currentUser;
-                if (user == null) {
-                  Scaffold.of(context).showSnackBar(const SnackBar(
-                    content: Text('No one has signed in.'),
-                  ));
-                  return;
-                }
-                _signOut();
-                final String uid = user.uid;
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text(uid + ' has successfully signed out.'),
-                ));
-              },
-            );
-          })
-        ],
-      ),
       body: Builder(builder: (BuildContext context) {
         return ListView(
           scrollDirection: Axis.vertical,
           children: <Widget>[
             _EmailPasswordForm(),
-            //_EmailLinkSignInSection(),
-            //_AnonymouslySignInSection(),
             //_GoogleSignInSection(),
-            // _PhoneSignInSection(Scaffold.of(context)),
-            // _OtherProvidersSignInSection(),
           ],
         );
       }),
@@ -102,124 +43,135 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
   final TextEditingController _passwordController = TextEditingController();
   bool _success;
   String _userEmail;
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            child: const Text(
-              '이약모약',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-              labelText: '아이디',
-              labelStyle: TextStyle(color: Colors.grey, fontSize: 14.0),
-            ),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _passwordController,
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-              labelText: '비밀번호',
-              labelStyle: TextStyle(color: Colors.grey, fontSize: 14.0),
-            ),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 30.0),
-          SizedBox(
-            //padding: const EdgeInsets.symmetric(vertical: 16.0),
-            //alignment: Alignment.center,
-            width: 350.0,
-            height: 45.0,
-            child: RaisedButton(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  _signInWithEmailAndPassword();
-                }
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 60, 16, 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
               child: const Text(
-                '로그인',
-                style: TextStyle(color: Colors.white),
+                '이약모약',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              color: Colors.teal[400],
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.center,
             ),
-          ),
-          /*new SizedBox(
-            width: 200.0,
-            height: 100.0,
-            child: new RaisedButton(
-              child: const Text('로그인', style: TextStyle(color: Colors.white),),
-              color: Colors.teal[400],
-              onPressed: (){},
+            SizedBox(
+              height: 20,
             ),
-          ),*/
-          Container(
-            child: FlatButton(
-              child: Text(
-                '아이디/비밀번호 찾기',
-                style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    fontSize: 13.0,
-                    color: Colors.grey[400]),
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                labelText: '아이디',
+                labelStyle: TextStyle(color: Colors.grey, fontSize: 14.0),
               ),
-              onPressed: () {}, //아이디 비밀번호 찾기 페이지로 이동
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-          ),
-          Container(
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                '혹시 이약모약 사용이 처음이신가요?',
-                style: TextStyle(fontSize: 12.0, color: Colors.grey[400]),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                labelText: '비밀번호',
+                labelStyle: TextStyle(color: Colors.grey, fontSize: 14.0),
               ),
-              FlatButton(
-                onPressed: () =>
-                    _pushPage(context, RegisterFirstPage()), //register page로 이동
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 50.0),
+            SizedBox(
+              //padding: const EdgeInsets.symmetric(vertical: 16.0),
+              //alignment: Alignment.center,
+              width: 400.0,
+              height: 45.0,
+              child: RaisedButton(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    _signInWithEmailAndPassword();
+                  }
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0)),
+                child: const Text(
+                  '로그인',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.teal[400],
+              ),
+            ),
+            Container(
+              child: FlatButton(
                 child: Text(
-                  '회원가입',
+                  '아이디/비밀번호 찾기',
                   style: TextStyle(
                       decoration: TextDecoration.underline,
-                      fontSize: 12.0,
-                      color: Colors.black),
+                      fontSize: 13.0,
+                      color: Colors.grey[400]),
                 ),
+                onPressed: () {}, //아이디 비밀번호 찾기 페이지로 이동
               ),
-            ]),
-          ),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              _success == null
-                  ? ''
-                  : (_success
-                      ? 'Successfully signed in ' + _userEmail
-                      : 'Sign in failed'),
-              style: TextStyle(color: Colors.red),
             ),
-          )
-        ],
+            Container(
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  '혹시 이약모약 사용이 처음이신가요?',
+                  style: TextStyle(fontSize: 12.0, color: Colors.grey[400]),
+                ),
+                FlatButton(
+                  onPressed: () => _pushPage(
+                      context, RegisterFirstPage()), //register page로 이동
+                  child: Text(
+                    '회원가입',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 12.0,
+                        color: Colors.black),
+                  ),
+                ),
+              ]),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                _success == null
+                    ? ''
+                    : (_success
+                        ? 'Successfully signed in ' + _userEmail
+                        : 'Sign in failed'),
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+
+            /* 로그인 뛰어넘기 */
+            IconButton(
+              icon: Icon(Icons.skip_next),
+              color: Colors.redAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, '/bottom_bar');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
