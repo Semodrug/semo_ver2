9e6ff299cdf78b93be965941b88d81daaa46afbd
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2020-12-31 16:34:42 +0900

    [UPDATE] UserData

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,12 +6,23 @@ class TheUser {
 
 class UserData {
   final String uid;
+
   final String name;
   final String sex;
+  final String birth;
+  // final String nickname;
+
   final String phone;
-  final bool birth;
+
+  final bool isPragnant;
   final List diseaseList;
 
   UserData(
-      this.uid, this.name, this.sex, this.phone, this.birth, this.diseaseList);
+      {this.uid,
+      this.name,
+      this.sex,
+      this.birth,
+      this.phone,
+      this.isPragnant,
+      this.diseaseList});
 }
