de9c1d849b6d6490c6ae341a0a800e7552cad1d0
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2021-01-09 13:33:45 +0900

    [CREATE] inquiry.dart

---
+++ lib/mypage/inquiry.dart
@@ -0,0 +1,43 @@
+// import 'package:flutter/material.dart';
+//
+// class Inquiry extends StatefulWidget {
+//   @override
+//   _InquiryState createState() => _InquiryState();
+// }
+//
+// class _InquiryState extends State<Inquiry> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: IconButton(
+//           icon: Icon(
+//             Icons.arrow_back,
+//             color: Colors.teal[200],
+//           ),
+//           onPressed: () => Navigator.pop(context),
+//         ),
+//         centerTitle: true,
+//         title: Text(
+//           '1:1 문의',
+//           style: TextStyle(
+//               fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
+//         ),
+//         elevation: 0,
+//         flexibleSpace: Container(
+//           decoration: BoxDecoration(
+//               gradient: LinearGradient(
+//                   begin: Alignment.topCenter,
+//                   end: Alignment.bottomCenter,
+//                   colors: <Color>[
+//                     Color(0xFFE9FFFB),
+//                     Color(0xFFE9FFFB),
+//                     Color(0xFFFFFFFF),
+//                   ])),
+//         ),
+//       ),
+//       backgroundColor: Colors.white,
+//       body: ,
+//     );
+//   }
+// }
