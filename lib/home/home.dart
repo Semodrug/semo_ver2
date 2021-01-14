3bc5bd024cc2059fb2ea78804a7859c8ed5052d0
3104kdy <3104kdy@naver.com>
2021-01-14 21:53:18 +0900

    [UPDATE] User media Query and padding

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -13,7 +13,7 @@ import 'package:semo_ver2/drug_info/phil_info.dart';
 import 'package:semo_ver2/shared/image.dart';
 
 /*약들의 개수를 length 만큼 보여주고 싶은데 그 length의 인덱스를 어떻게 넘겨주지..?*/
-int check = 0;
+int num = 0;
 
 class HomePage extends StatefulWidget {
   String appBarForSearch;
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     //widget.appBarForSearch = false;
 
-    if(widget.appBarForSearch == 'search'){
+    if (widget.appBarForSearch == 'search') {
       return Scaffold(
         appBar: AppBar(
           // centerTitle: true,
@@ -37,7 +37,9 @@ class _HomePageState extends State<HomePage> {
           title: Text(
             '이약모약',
             style: TextStyle(
-                fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black),
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
           ),
           actions: [
             IconButton(
@@ -57,24 +59,21 @@ class _HomePageState extends State<HomePage> {
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                     colors: <Color>[
-                      Color(0xFFE9FFFB),
-                      Color(0xFFE9FFFB),
-                      Color(0xFFFFFFFF),
-                    ])),
+                  Color(0xFFE9FFFB),
+                  Color(0xFFE9FFFB),
+                  Color(0xFFFFFFFF),
+                ])),
           ),
         ),
         backgroundColor: Colors.white,
         body: _buildBody(context),
       );
-    }
-    else {
+    } else {
       return Scaffold(
         backgroundColor: Colors.white,
         body: _buildBody(context),
       );
     }
-
-
   }
 
   Widget _buildBody(BuildContext context) {
@@ -90,32 +89,32 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildList(BuildContext context, List<SavedDrug> snapshot) {
-    check = 0;
+    num = 0;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
 
     int count = snapshot.length;
     return Column(
       children: [
         SearchBar(),
         Container(
-          margin: EdgeInsets.only(left: 19, top: 6, bottom: 2),
+          margin: EdgeInsets.only(left: 16, top: 0, bottom: 0, right: 16),
           child: Row(
             children: <Widget>[
-              Text('나의 상비약'), // theme 추가
-              ButtonTheme(
-                //padding: EdgeInsets.symmetric(vertical: 0, horizontal: 2),
-                height: 10,
-                minWidth: 20,
-                child: FlatButton(
-                  child: Text(
-                    count.toString(),
-                    style: TextStyle(color: Colors.teal[400], fontSize: 12.0),
-                  ),
-                ),
+              Row(
+                children: [
+                  Text('나의 상비약'),
+                  SizedBox(width: 10),
+                  Text(count.toString())
+                ],
               ),
-              SizedBox(
-                width: 180,
-              ),
-              FlatButton(
+              // theme 추가
+
+//              SizedBox(
+//                width: width/2.3
+//              ),
+              Spacer(),
+              TextButton(
                 child: Text(
                   '편집',
                 ),
@@ -131,48 +130,48 @@ class _HomePageState extends State<HomePage> {
           thickness: 1,
         ),
         Expanded(
-          child: ListView(
-            padding: EdgeInsets.all(10.0),
+          child:
+
+          ListView(
+            //padding: EdgeInsets.symmetric(horizontal : 20.0),
             children:
-            snapshot.map((data) => _buildListItem(context, data)).toList(),
+                snapshot.map((data) => _buildListItem(context, data)).toList(),
           ),
         ),
-        ButtonTheme(
-          padding: EdgeInsets.fromLTRB(50, 0, 5, 15),
-          minWidth: 340.0,
-          height: 70.0,
+        Container(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          width: double.infinity, //width/33 * 28.7,
+          height: height / 10,
           //buttonColor: Colors.redAccent,
-          child: RaisedButton.icon(
-            color: Colors.white70,
+          child: FlatButton.icon(
+            color: Color(0XFFEEEEEE),
             icon: Icon(Icons.add),
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => AddButton()));
             },
-            padding: EdgeInsets.fromLTRB(20, 5, 5, 15),
-            label: Container(
-              width: 340,
-              padding: const EdgeInsets.all(10.0),
-              child: const Text('상비약 추가하기'),
-            ),
+            //padding: EdgeInsets.fromLTRB(20, 5, 5, 15),
+            label: Text('상비약 추가하기'),
           ),
-        )
+        ),
       ],
     );
   }
 
+  Widget _buildListItem(BuildContext context, SavedDrug data) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    num++;
 
-  Widget _buildListItem(BuildContext context, SavedDrug data ) {
-    check++;
     /*너무 긴 이름들 잘라서 보여주기 정보를 바꾸는 건 아님*/
     String _checkLongName(SavedDrug data) {
       String newName = data.itemName;
       List splitName = [];
-      if (data.itemName.contains('(')) {
+      if (data.itemName.contains('(') || data.itemName.contains('(군납') ) {
         newName = data.itemName.replaceAll('(', '(');
         if (newName.contains('')) {
           splitName = newName.split('(');
-          //print(splitName);
+          print(splitName);
           newName = splitName[0];
         }
       }
@@ -198,57 +197,68 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       },
-      child: Container(
-        width: double.infinity,
-        height: 100.0,
-        child: Material(
-          color: Colors.white,
-          child:
-          Row(
-            children: [
-              SizedBox(
-                width: 15,
-                child: Center(
-                  child: Text(check.toString()),
-                ),
-              ),
-              Container(
-                  padding: EdgeInsets.fromLTRB(5, 0, 5, 5),
-                  child: SizedBox(
-                      width: 88, height: 66, child: DrugImage(drugItemSeq: data.itemSeq))),
-              Container(
-                  padding: EdgeInsets.fromLTRB(15, 20, 5, 5),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(children: [
-                        Text(
-                          _checkLongName(data),
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        )
-                      ]),
-                      Expanded(
-                          child:
-                          Row(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            width: double.infinity,
+            height: 100.0,
+            child: Material(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: width / 20,
+                    child: Center(
+                      child: Text(num.toString()),
+                    ),
+                  ),
+                  Container(
+                      //이미지는 고정값
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: SizedBox(
+                          height: 70,
+                          width: 88,
+                          child: AspectRatio(
+                              aspectRatio: 2.3 / 2,
+                              child: DrugImage(drugItemSeq: data.itemSeq)))),
+                  Container(
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(children: [
+                            Text(
+                              _checkLongName(data),
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            )
+                          ]),
+                          Expanded(
+                              child: Row(
                             children: [_categoryButton((_checkCategory(data)))],
                           )),
-                      SizedBox(
-                        height: 3,
-                      ),
-                      Expanded(
-                        child: Text(
-                          data.expiration,
-                          style: TextStyle(
-                              color: Colors.grey[600], fontSize: 13),
-                        ),
-                      )
-                    ],
-                  )),
-            ],
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Expanded(
+                            child: Text(
+                              data.expiration,
+                              style: TextStyle(
+                                  color: Colors.grey[600], fontSize: 13),
+                            ),
+                          )
+                        ],
+                      )),
+                ],
+              ),
+            ),
           ),
-        ),
+          Divider(
+            thickness: 1,
+          )
+        ],
       ),
     );
   }
@@ -274,30 +284,30 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class SearchBar extends StatefulWidget {
-  @override
-  _SearchBarState createState() => _SearchBarState();
-}
-
-class _SearchBarState extends State<SearchBar> {
+class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double searchWidth =  width / 33 * 30.1;
+    double searchHeight =  height / 20;
+
     //이 search bar에서 바로 검색을 하게 할 것인가?
     return Row(
       children: <Widget>[
         Container(
-          padding: EdgeInsets.only(
-            left: 10,
-          ),
-          margin: EdgeInsets.fromLTRB(0, 11, 0, 0),
+          padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
           child: SizedBox(
-              width: 390,
-              height: 35,
+              width: searchWidth,
+              height: searchHeight,
               child: FlatButton(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Icon(Icons.search, size: 20),
+                    Center(child: Icon(Icons.search, size: 20)),
+                    SizedBox(
+                      width: 10,
+                    ),
                     Text("어떤 약을 찾고 계세요? "),
                   ],
                 ),
@@ -319,9 +329,9 @@ class _SearchBarState extends State<SearchBar> {
                     borderRadius: new BorderRadius.circular(8.0)),
               )),
         ),
-        SizedBox(
-          width: 10,
-        ),
+//        SizedBox(
+//          width: 10,
+//        ),
       ],
     );
   }
