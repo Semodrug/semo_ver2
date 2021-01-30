31a89ce4a9d2e2853933e0fb0c2c176b4288cb3c
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2021-01-30 13:44:57 +0900

    [UPDATE] expiration UI

--- lib/drug_info/expiration_g.dart
+++ lib/drug_info/expiration_g.dart
@@ -9,6 +9,7 @@ import 'package:semo_ver2/services/db.dart';
 import 'package:semo_ver2/shared/category_button.dart';
 import 'package:semo_ver2/shared/loading.dart';
 import 'package:semo_ver2/shared/image.dart';
+import 'package:semo_ver2/theme/colors.dart';
 
 class ExpirationG extends StatefulWidget {
   final String drugItemSeq;
@@ -46,7 +47,7 @@ class _ExpirationGState extends State<ExpirationG> {
         ),
         centerTitle: true,
         title: Text(
-          '상비약 추가하기',
+          '약 추가하기',
           style: TextStyle(
               fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
         ),
@@ -72,26 +73,29 @@ class _ExpirationGState extends State<ExpirationG> {
 
           if (snapshot.hasData) {
             if (_isGeneral) {
-              return Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    _topInfo(context, drug),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    _datePickG(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+              return Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  _topInfo(context, drug),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: _datePickG(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         TextButton(
                           child: Text(
-                            '조제받은 약인가요?',
+                            '처방받은 약인가요?',
                             style: TextStyle(
-                              color: Colors.grey,
+                              color: gray500,
                               fontSize: 12.0,
                               fontWeight: FontWeight.w400,
                               decoration: TextDecoration.underline,
@@ -105,33 +109,38 @@ class _ExpirationGState extends State<ExpirationG> {
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    _okButton(context, user, drug, _expirationDateString)
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child:
+                        _okButton(context, user, drug, _expirationDateString),
+                  )
+                ],
               );
             } else {
-              return Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      SizedBox(height: 20),
-                      _topInfo(context, drug),
-                      SizedBox(height: 20),
-                      _datePick(),
-                      SizedBox(height: 20),
-                      Container(
-                          alignment: Alignment.bottomLeft,
-                          child: _durationPick()),
-                      SizedBox(height: 20),
-                      _isSelf ? _expirationPick() : Container(),
-                      SizedBox(height: 20),
-                      _expectedDuration(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    _topInfo(context, drug),
+                    SizedBox(height: 20),
+                    _datePick(),
+                    SizedBox(height: 20),
+                    Container(
+                        alignment: Alignment.bottomLeft,
+                        child: _durationPick()),
+                    SizedBox(height: 20),
+                    _isSelf ? _expirationPick() : Container(),
+                    // SizedBox(height: 20),
+                    _expectedDuration(),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           TextButton(
                             child: Text(
@@ -151,12 +160,16 @@ class _ExpirationGState extends State<ExpirationG> {
                           ),
                         ],
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      _okButton(context, user, drug, _expectedDateString)
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child:
+                          _okButton(context, user, drug, _expectedDateString),
+                    )
+                  ],
                 ),
               );
             }
@@ -183,14 +196,11 @@ class _ExpirationGState extends State<ExpirationG> {
         ),
         Text(
           drug.entpName,
-          style: TextStyle(
-            color: Colors.grey[600],
-          ),
+          style: Theme.of(context).textTheme.caption,
         ),
         Text(
           drug.itemName,
-          style: TextStyle(
-              color: Colors.black, fontSize: 16.0, fontWeight: FontWeight.bold),
+          style: Theme.of(context).textTheme.headline5,
         ),
         CategoryButton(str: drug.category),
       ],
@@ -202,8 +212,8 @@ class _ExpirationGState extends State<ExpirationG> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          '사용기한 입력하기',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          '사용기한',
+          style: Theme.of(context).textTheme.subtitle1,
         ),
         SizedBox(
           height: 4,
@@ -241,7 +251,7 @@ class _ExpirationGState extends State<ExpirationG> {
                 children: [
                   Text(
                     '${DateFormat('yyyy').format(_pickDateTime)}년 ${DateFormat('MM').format(_pickDateTime)}월 ${DateFormat('dd').format(_pickDateTime)}일',
-                    style: TextStyle(color: Colors.black),
+                    style: Theme.of(context).textTheme.bodyText2,
                   ),
                   Icon(Icons.keyboard_arrow_down)
                 ],
@@ -252,144 +262,166 @@ class _ExpirationGState extends State<ExpirationG> {
   }
 
   Widget _datePick() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          '약을 제조받은 일자가 언제인가요?',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        FlatButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              DatePicker.showDatePicker(context,
-                  showTitleActions: true,
-                  minTime: DateTime(2000, 1, 1),
-                  maxTime: DateTime(2030, 12, 31),
-                  onChanged: (date) {}, onConfirm: (date) async {
-                setState(() {
-                  _pickDateTime = date;
-                });
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '언제 약을 제조받으셨나요?',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          FlatButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                DatePicker.showDatePicker(context,
+                    showTitleActions: true,
+                    minTime: DateTime(2000, 1, 1),
+                    maxTime: DateTime(2030, 12, 31),
+                    onChanged: (date) {}, onConfirm: (date) async {
+                  setState(() {
+                    _pickDateTime = date;
+                  });
+                },
+                    currentTime: DateTime.now(),
+                    locale: LocaleType.ko); // need currentTime setting?
               },
-                  currentTime: DateTime.now(),
-                  locale: LocaleType.ko); // need currentTime setting?
-            },
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 16,
-              ),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(4)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '${DateFormat('yyyy').format(_pickDateTime)}년 ${DateFormat('MM').format(_pickDateTime)}월 ${DateFormat('dd').format(_pickDateTime)}일',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  Icon(Icons.keyboard_arrow_down)
-                ],
-              ),
-            ))
-      ],
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '${DateFormat('yyyy').format(_pickDateTime)}년 ${DateFormat('MM').format(_pickDateTime)}월 ${DateFormat('dd').format(_pickDateTime)}일',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    Icon(Icons.keyboard_arrow_down)
+                  ],
+                ),
+              ))
+        ],
+      ),
     );
   }
 
   Widget _durationPick() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          '약 종류에 따른 예상 사용가능기간을 선택해주세요',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '약 종류에 따른 예상 사용가능기간을 선택해주세요',
+            style: Theme.of(context).textTheme.subtitle1,
           ),
-          height: 45,
-          // width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(4)),
-          child: DropdownButton(
-            isExpanded: true,
-            icon: Icon(Icons.keyboard_arrow_down),
-            underline: SizedBox.shrink(),
-            value: _durationInt,
-            items: <DropdownMenuItem>[
-              DropdownMenuItem(
-                value: 60,
-                child: Text('[2개월] 약국에서 처방받은 알약'),
-                onTap: () {
-                  setState(() {
-                    _isSelf = false;
-                  });
-                },
-              ),
-              DropdownMenuItem(
-                value: 14,
-                child: Text('[2주] 개봉된 액체상태의 시럽'),
-                onTap: () {
-                  setState(() {
-                    _isSelf = false;
-                  });
-                },
-              ),
-              DropdownMenuItem(
-                value: 30,
-                child: Text('[1개월] 개봉된 액체상태의 안약'),
-                onTap: () {
-                  setState(() {
-                    _isSelf = false;
-                  });
-                },
-              ),
-              DropdownMenuItem(
-                value: 180,
-                child: Text('[6개월] 개봉된 연고'),
-                onTap: () {
-                  setState(() {
-                    _isSelf = false;
-                  });
-                },
-              ),
-              DropdownMenuItem(
-                value: 0,
-                child: Text('직접 입력하기'),
-                onTap: () {
-                  setState(() {
-                    _isSelf = true;
-                  });
-                },
-              ),
-            ],
-            onChanged: (value) {
-              setState(() {
-                _durationInt = value;
-                DateTime _expectedDateTime =
-                    _pickDateTime.add(Duration(days: _durationInt));
-                _expectedDateString =
-                    DateFormat('yyyy.MM.dd').format(_expectedDateTime);
-              });
-            },
+          SizedBox(
+            height: 4,
           ),
-        ),
-      ],
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 16,
+            ),
+            height: 45,
+            // width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(4)),
+            child: DropdownButton(
+              isExpanded: true,
+              icon: Icon(Icons.keyboard_arrow_down),
+              underline: SizedBox.shrink(),
+              value: _durationInt,
+              items: <DropdownMenuItem>[
+                DropdownMenuItem(
+                  value: 60,
+                  child: Text(
+                    '[2개월] 약국에서 처방받은 알약',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isSelf = false;
+                    });
+                  },
+                ),
+                DropdownMenuItem(
+                  value: 14,
+                  child: Text(
+                    '[2주] 개봉된 액체상태의 시럽',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isSelf = false;
+                    });
+                  },
+                ),
+                DropdownMenuItem(
+                  value: 30,
+                  child: Text(
+                    '[1개월] 개봉된 액체상태의 안약',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isSelf = false;
+                    });
+                  },
+                ),
+                DropdownMenuItem(
+                  value: 180,
+                  child: Text(
+                    '[6개월] 개봉된 연고',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isSelf = false;
+                    });
+                  },
+                ),
+                DropdownMenuItem(
+                  value: 0,
+                  child: Text(
+                    '직접 입력하기',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isSelf = true;
+                    });
+                  },
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  _durationInt = value;
+                  DateTime _expectedDateTime =
+                      _pickDateTime.add(Duration(days: _durationInt));
+                  _expectedDateString =
+                      DateFormat('yyyy.MM.dd').format(_expectedDateTime);
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _expectedDuration() {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
         RichText(
           textAlign: TextAlign.center,
@@ -403,20 +435,22 @@ class _ExpirationGState extends State<ExpirationG> {
             children: <TextSpan>[
               TextSpan(
                 text: '예상 유효기간은 ',
+                style: Theme.of(context).textTheme.bodyText2,
               ),
               TextSpan(
                   text: _expectedDateString,
                   style: TextStyle(
                       fontWeight: FontWeight.bold, color: Colors.teal[400])),
               TextSpan(
-                text: '입니다',
+                text: ' 입니다',
+                style: Theme.of(context).textTheme.bodyText2,
               ),
             ],
           ),
         ),
-        SizedBox(
-          height: 20,
-        ),
+        // SizedBox(
+        //   height: 20,
+        // ),
       ],
     );
   }
