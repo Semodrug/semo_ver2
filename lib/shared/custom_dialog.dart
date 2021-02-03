59bdb93c20c3a8ca6d9687b5db4157e3ddd78394
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2021-02-03 10:18:50 +0900

    [UPDATE] custom dialog UI

--- lib/shared/custom_dialog.dart
+++ lib/shared/custom_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:semo_ver2/services/auth.dart';
 import 'package:semo_ver2/theme/colors.dart';
 
 class CustomDialog {
@@ -9,55 +8,64 @@ class CustomDialog {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
+          contentPadding: EdgeInsets.all(16),
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          // title:
           content: Column(
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 18),
+              SizedBox(height: 16),
+              /* BODY */
               Text(bodyString,
                   style: Theme.of(context)
                       .textTheme
                       .bodyText1
                       .copyWith(color: gray700)),
-              SizedBox(height: 24),
+              SizedBox(height: 28),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  /* LEFT ACTION BUTTON */
                   ElevatedButton(
                     child: Text(
                       actionName1,
-                      style: TextStyle(fontSize: 12, color: primary400_line),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(color: primary400_line),
                     ),
                     style: ElevatedButton.styleFrom(
-                        minimumSize: Size(100, 40),
+                        minimumSize: Size(120, 40),
                         padding: EdgeInsets.symmetric(horizontal: 10),
                         elevation: 0,
                         primary: gray50,
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0),
+                            borderRadius: BorderRadius.circular(8.0),
                             side: BorderSide(color: gray75))),
                     onPressed: () {
                       Navigator.pop(context);
                     },
                   ),
                   SizedBox(width: 16),
+                  /* RIGHT ACTION BUTTON */
                   ElevatedButton(
                     child: Text(
                       actionName2,
-                      style: TextStyle(fontSize: 12, color: gray0_white),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(color: gray0_white),
                     ),
                     style: ElevatedButton.styleFrom(
-                        minimumSize: Size(100, 40),
+                        minimumSize: Size(120, 40),
                         padding: EdgeInsets.symmetric(horizontal: 10),
                         elevation: 0,
                         primary: primary300_main,
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0),
-                            side: BorderSide(color: gray75))),
+                            borderRadius: BorderRadius.circular(8.0),
+                            side: BorderSide(color: primary400_line))),
                     onPressed: () async {
                       if (actionCode == 'logout') {
                         action(context);
