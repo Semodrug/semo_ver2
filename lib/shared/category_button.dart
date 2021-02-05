0431922fd46e88a99cf83d255ae61b6706d666ed
3104kdy <3104kdy@naver.com>
2021-02-05 14:11:55 +0900

    [UPDATE] For different color in search result list tile

--- lib/shared/category_button.dart
+++ lib/shared/category_button.dart
@@ -4,9 +4,10 @@ import 'package:semo_ver2/theme/colors.dart';
 class CategoryButton extends StatelessWidget {
   final String str;
   String fromHome = '';
+  final bool forsearch;
 
   //const
-  CategoryButton({this.str, this.fromHome}); // : super(key: key);
+  CategoryButton({this.str, this.fromHome, this.forsearch}); // : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +29,28 @@ class CategoryButton extends StatelessWidget {
       return newName;
     }
 
+    if(forsearch == true){
+      return ElevatedButton(
+        child: Text(
+          '${_shortenCategory(str)}',
+          style: Theme.of(context).textTheme.caption.copyWith(color: gray500)
+        ),
+        style: ElevatedButton.styleFrom(
+            minimumSize: Size(12, 23),
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            elevation: 0,
+            primary: gray50,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0),
+                side: BorderSide(color: gray75))),
+        onPressed: () {},
+      );
+    }
+
     return ElevatedButton(
       child: Text(
         '${_shortenCategory(str)}',
-        style: TextStyle(fontSize: 12, color: primary600_bold_text),
+          style: Theme.of(context).textTheme.subtitle2.copyWith(color: primary600_bold_text)
       ),
       style: ElevatedButton.styleFrom(
           minimumSize: Size(12, 23),
