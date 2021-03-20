643779e48ef4170f473483bfa975e55adb5d35ef
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2021-03-20 20:19:46 +0900

    [UPDATE] barcode flow

--- lib/bottom_bar.dart
+++ lib/bottom_bar.dart
@@ -1,7 +1,5 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:semo_ver2/camera/barcode.dart';
-import 'package:semo_ver2/camera/case.dart';
 import 'package:semo_ver2/camera/camera_page.dart';
 import 'package:semo_ver2/home/home.dart';
 import 'package:semo_ver2/ranking/ranking.dart';
