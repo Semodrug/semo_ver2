da248aee2026fe860e4d06a4f63c44b0af50d6d1
Sumi Lee <40039201+underhiswings@users.noreply.github.com>
2021-03-20 19:31:41 +0900

    [UPDATE] camera flow

--- lib/bottom_bar.dart
+++ lib/bottom_bar.dart
@@ -1,6 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:semo_ver2/camera/barcode.dart';
+import 'package:semo_ver2/camera/case.dart';
+import 'package:semo_ver2/camera/camera_page.dart';
 import 'package:semo_ver2/home/home.dart';
 import 'package:semo_ver2/ranking/ranking.dart';
 import 'package:semo_ver2/shared/appbar.dart';
@@ -67,22 +69,190 @@ class _BottomBarState extends State<BottomBar> {
           elevation: 0,
           child: Image.asset('assets/icons/bottom_camera.png'),
           onPressed: () async {
-            // 디바이스에서 이용가능한 카메라 목록을 받아옵니다.
-            final cameras = await availableCameras();
-
-            // 이용가능한 카메라 목록에서 특정 카메라를 얻습니다.
-            final firstCamera = cameras.first;
-
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CameraPage(
-                  camera: firstCamera,
-                ),
-              ),
-            );
+            showModalBottomSheet(
+                backgroundColor: Colors.transparent,
+                context: context,
+                builder: (BuildContext context) {
+                  return _popUpAddDrug(context);
+                });
           },
         ),
+      ),
+    );
+  }
+
+  Widget _popUpAddDrug(context) {
+    return Container(
+      //color: yellow,
+      decoration: new BoxDecoration(
+          color: Colors.white,
+          borderRadius: new BorderRadius.only(
+            topLeft: const Radius.circular(12.0),
+            topRight: const Radius.circular(12.0),
+          )),
+      child: Wrap(
+        children: <Widget>[
+          Container(
+            height: 45,
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Container(
+                    child: Text(
+                      '약 추가하기',
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle1
+                          .copyWith(color: primary500_light_text),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: MaterialButton(
+              onPressed: () async {
+                // 디바이스에서 이용가능한 카메라 목록을 받아옵니다.
+                final cameras = await availableCameras();
+
+                // 이용가능한 카메라 목록에서 특정 카메라를 얻습니다.
+                final firstCamera = cameras.first;
+
+                Navigator.pop(context);
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CameraPage(
+                      camera: firstCamera,
+                      initial: 0,
+                    ),
+                  ),
+                );
+              },
+              child: Row(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: Image.asset('assets/icons/barcode_icon_grey.png'),
+                  ),
+                  SizedBox(width: 15),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        "바코드 인식",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .copyWith(color: gray900),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: MaterialButton(
+              onPressed: () async {
+                // 디바이스에서 이용가능한 카메라 목록을 받아옵니다.
+                final cameras = await availableCameras();
+
+                // 이용가능한 카메라 목록에서 특정 카메라를 얻습니다.
+                final firstCamera = cameras.first;
+
+                Navigator.pop(context);
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CameraPage(
+                      camera: firstCamera,
+                      initial: 1,
+                    ),
+                  ),
+                );
+              },
+              child: Row(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: Image.asset('assets/icons/case_icon_grey.png'),
+                  ),
+                  SizedBox(width: 15),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        "케이스 인식",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .copyWith(color: gray900),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: MaterialButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/search');
+              },
+              child: Row(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: Image.asset('assets/icons/search_grey.png'),
+                  ),
+                  SizedBox(width: 15),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        "약 이름 검색",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .copyWith(color: gray900),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Divider(thickness: 1, color: gray100),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: MaterialButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Center(
+                    child: Text("닫기",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .copyWith(color: gray300_inactivated)))),
+          )
+        ],
       ),
     );
   }
